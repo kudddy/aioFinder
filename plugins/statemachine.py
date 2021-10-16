e1d20f776a91c996a98853a6038168720ec2d8f7
16783869 <Khudyakov.K.Ko@sberbank.ru>
2021-10-16 15:19:22 +0300

    тесты теперь работают как надо + небольшой рефакторинг

--- plugins/statemachine.py
+++ plugins/statemachine.py
@@ -7,12 +7,7 @@ from plugins.systems import Systems
 from plugins.systems import LocalCacheForCallbackFunc
 from plugins.tools.tokenizer import QueryBuilder
 from plugins.mc.init import AioMemCache
-from plugins.classifier import Model
-from plugins.callback import hello_message, get_vacancy, \
-    add_vacancy_to_favorites, history_viewed_vacancy, \
-    viewed_history_already_cleaned, add_vacancy_to_history, \
-    show_favor_vacancy, return_to_init_state, \
-    uncover_vacancy_info, get_extend_vacancy, nothing_found
+from plugins.configurator import model, state
 
 
 class Stages:
@@ -65,36 +60,6 @@ class Stages:
 
 
 async def init_stages(app: Application):
-    # требуется внедрить классификатор входящих интентов, которые возвращают стейт сценари
-    train_data: dict = {
-        "Следующая": 1,
-        "В избранное": 2,
-        "Очистить": 3,
-        "Очистил": 4,
-        "Добавил": 5,
-        "Избранное": 6,
-        "В начало": 0,
-        "Раскрыть": 8,
-        "Расширенный": 9
-    }
-    # коллбэк функции
-    model = Model()
-
-    model.fit(train_data)
-
-    state = {
-        0: hello_message,
-        1: get_vacancy,
-        2: add_vacancy_to_favorites,
-        3: history_viewed_vacancy,
-        4: viewed_history_already_cleaned,
-        5: add_vacancy_to_history,
-        6: show_favor_vacancy,
-        7: return_to_init_state,
-        8: uncover_vacancy_info,
-        9: get_extend_vacancy,
-        10: nothing_found
-    }
     # коннектор к базе данных
     mc = AioMemCache(app['mc'])
     # инициализация локального кэша для реализации возможности кэшировать запросы к базе данных
